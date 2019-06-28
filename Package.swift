38266bd4d41096a9a60ac6803c1473d4b6e1c23d
Hanzhou Shi <hanzhou87@gmail.com>
2019-06-27 21:30:32 -0700

    Fixing linux posix_spawn_file_actions_t initialization issue.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,8 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftPawn",
-            targets: ["SwiftPawn"]),
+            targets: ["SwiftPawn"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -21,9 +22,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftPawn",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "SwiftPawnTests",
-            dependencies: ["SwiftPawn"]),
+            dependencies: ["SwiftPawn"]
+        ),
     ]
 )
--- Tests/SwiftPawnTests/SwiftPawnTests.swift
+++ Tests/SwiftPawnTests/SwiftPawnTests.swift
@@ -2,22 +2,22 @@
 import XCTest
 
 final class SwiftPawnTests: XCTestCase {
-  func testExecute() {
-    do {
-      let (status, out, err) = try SwiftPawn.execute(command: "git", arguments: ["git", "status"])
-      print("------ status ------")
-      print(status)
-      print("------ stdout ------")
-      print(out)
-      print("------ stderr ------")
-      print(err)
-      print("--------------------")
-    } catch {
-      print(error)
+    func testExecute() {
+        do {
+            let (status, out, err) = try SwiftPawn.execute(command: "git", arguments: ["git", "status"])
+            print("------ status ------")
+            print(status)
+            print("------ stdout ------")
+            print(out)
+            print("------ stderr ------")
+            print(err)
+            print("--------------------")
+        } catch {
+            print(error)
+        }
     }
-  }
 
-  static var allTests = [
-    ("testExecute", testExecute),
-  ]
+    static var allTests = [
+        ("testExecute", testExecute),
+    ]
 }
